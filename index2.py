06b33d9d8e3d0497850e7b7eeb316ab5f61a6ca7
berg.feng <fengjutian>
2019-05-25 19:50:58 +0800

    save

---
+++ index2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt5.QtWidgets import (QWidget, QToolTip, QPushButton, QApplication)
+from PyQt5.QtGui import QFont
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        QToolTip.setFont(QFont('SanSerif', 10))
+        self.setToolTip('This is a <b>QWidget</b>widget')
+
+        btn = QPushButton('Button', self)
+        btn.setToolTip('This is a <b>QPushButton</b>widget')
+        btn.resize(btn.sizeHint())
+        btn.move(0, 0)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Tooltips')
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
